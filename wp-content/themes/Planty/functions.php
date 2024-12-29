539745078c6d8c195529494449fc5234233f9a35
Pifpafplouff <gaonachalan@gmail.com>
2024-12-29 18:32:35 +0100

    arguments

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -7,7 +7,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
-function ajout_element_menu($items, $args) {
+function ajout_element_menu($items) {
     if (is_user_logged_in() ) {//&& $args->theme_location == 'menu-1' { {
         $adminUrl = get_admin_url();
         $items .= "<li id=\"menu-item-193\"  class=\"menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu\"><a id=\"lien-admin\" href=\"{$adminUrl}\">Admin</a></li>";
@@ -18,7 +18,7 @@ function ajout_element_menu($items, $args) {
 }
 
 
-add_filter('wp_nav_menu_items', 'ajout_element_menu', 10, 2);
+add_filter('wp_nav_menu_items', 'ajout_element_menu', 10, 1);
 
 
 ?>
